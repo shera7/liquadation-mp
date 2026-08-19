eb16abc8638b20a44b3322ba1d1355b045bbf4b8
shera7 <sherbek728@gmail.com>
2026-08-19 10:40:58 +0500

    Update page.tsx

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import NewProductForm from "@/components/admin/NewProductForm";
-
+export const dynamic = "force-dynamic";
 export default async function NewProductPage() {
   const categories = await prisma.category.findMany({ orderBy: { name: "asc" } });
 
