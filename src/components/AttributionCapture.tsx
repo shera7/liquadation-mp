84c91f44705b44248c615bdae2fe45bfa0e18fad
shera7 <sherbek728@gmail.com>
2026-09-21 09:30:43 +0500

    Add AttributionCapture component for tracking attribution

---
+++ src/components/AttributionCapture.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect } from "react";
+import { captureAttribution } from "@/lib/attribution";
+
+export default function AttributionCapture() {
+  useEffect(() => {
+    captureAttribution();
+  }, []);
+  return null;
+}
